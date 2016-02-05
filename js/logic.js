0a80548a19f216ec2557d3604dd649e610f401b2
Irene Araya <iarayal>
2016-02-05 15:59:33 -0600

    first commit

--- js/logic.js
+++ js/logic.js
@@ -1,46 +1,14 @@
-angular.module('todoList', [])
-    .controller('ToDoCtrl', function() {
+angular.module('todoList', ['ngRoute','todoList.controllers'])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
 
-        this.tasksCol = [];
-
-        this.addTask = function () {
-            console.clear();
-
-            if (this.description !== undefined && this.dueDate !== undefined) {
-                var itemTask = {};
-                itemTask.description = this.description;
-                itemTask.dueDate = this.dueDate;
-                itemTask.done = false;
-
-                this.tasksCol.push(itemTask);
-            }
-        };
-
-        this.clearDoneTasks = function () {
-            var listContainer = document.querySelector("#todoList");
-            var taskItems = listContainer.querySelectorAll("li");
-
-            for (var i = 0; i < taskItems.length; i++) {
-                var child = taskItems[i];
-
-                if (child.getAttribute("data-status") === "true") {
-                    listContainer.removeChild(child);
-                }
-            };
-        };
-
-        this.setDone = function (taskID) {
-            this.tasksCol[taskID].done = document.querySelector("input#taskDone-" + taskID).checked ? true : false;
-        };
-
-        this.getTaskNumbers = function () {
-            var n = 0;
-
-            for (var i = 0; i < this.tasksCol.length; i++) {
-                var current = this.tasksCol[i];
-                n += current.done ? 0 : 1;
-            };
-
-            return n;
-        }
+    .when('/', {
+        templateUrl: 'views/taskList.html',
+    })
+    .when('/description', {
+        templateUrl: 'views/taskDescription.html'
+    })
+    .otherwise({
+        redirectTo: '/'
     });
+}]);
